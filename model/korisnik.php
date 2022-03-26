4f539b3010d020b71c97184f385567c0d4a46d05
UrosVesic <uros99uki@gmail.com>
2022-03-26 16:05:15 +0100

    Implementirana klasa korisnik

--- model/korisnik.php
+++ model/korisnik.php
@@ -1,1 +1,21 @@
+<?php
 
+class Korisnik
+{
+    public $username;
+    public $password;
+    public $imePrezime;
+
+    public function __construct($username = null, $password = null, $imePrezime = null,)
+    {
+        $this->username = $username;
+        $this->password = $password;
+        $this->imePrezime = $imePrezime;
+    }
+
+    public static function prijaviSe($usr, mysqli $conn)
+    {
+        $query = "SELECT * FROM user WHERE username='$usr->username' and password='$usr->password'";
+        return $conn->query($query);
+    }
+}
