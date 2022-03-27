8247353bf1b45c33edf410d404b028af03580c3c
UrosVesic <uros99uki@gmail.com>
2022-03-27 16:18:04 +0200

    dodata funkcionalnost azuriranja

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $('#dodaj').submit(function(){
         const $inputs = $form.find('input, select, button, textarea');
         const serijalizacija = $form.serialize();
 
-        request = $.ajax({
+    request = $.ajax({
         url:'handler/add.php',
         type:'post',
         data:serijalizacija
@@ -25,7 +25,28 @@ $('#dodaj').submit(function(){
         console.error('Sledeca greska se desila: '+textStatus, errorThrown)
     });
 
-}
+    }
+
+    if($btn.is('button[id="azuriraj"]')){
+         event.preventDefault();
+        const $inputs = $form.find('input, select, button, textarea');
+        const serijalizacija = $form.serialize();
+        const red = $('input[name=checked-donut]:checked');
+        console.log(serijalizacija);
+
+    request = $.ajax({
+        url:'handler/update.php',
+        type:'post',
+        data: serijalizacija
+    });
+    request.done(function(response, textStatus, jqXHR){
+        if(response==="Success"){
+            alert("Sportista je azuriran");
+            location.reload(true);
+        }
+        else console.log("Sportista nije azuriran "+ response);
+    });
+    }
 });
 
 $('#obrisi').click(function(){
@@ -49,7 +70,5 @@ $('#obrisi').click(function(){
     });
 });
 
-$('#azuriraj').click(function(){
-    
-});
+
 
